631e72a24e388d4764e637e10f5ea53253e7c364
ethanius <jakub.kraft@gmail.com>
2019-12-13 21:44:01 +0100

    polyfilly primo z core-js a ne z babel/polyfill, ktery zastaral

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 import './index.less';
 
 ReactDOM.render(<App />, document.getElementById('app'));
