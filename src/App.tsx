a9850f4a5af7940ad000d603e86179a336f21b62
ethanius <jakub.kraft@gmail.com>
2019-12-19 21:32:14 +0100

    jiny testovaci obrazek

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App = () => {
 		<div>
 			<h1>Hello <span>{name}</span>!</h1>
 			<Suspense fallback={<div>Loading...</div>}>
-				<LazyImage src="https://s3-us-west-2.amazonaws.com/media.brothers-brick.com/wp-content/uploads/2018/12/LEGO-Creator-Expert-10264-Corner-Garage-36-1024x542.jpg" />
+				<LazyImage src="https://satyr.io/1200x16:9" />
 			</Suspense>
 		</div>
 	);
